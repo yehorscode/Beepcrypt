04e9df6788df40ffcf0e5a519578f0feccbbd7a6
Yehor <omelianenkoegor@gmail.com>
2025-06-21 15:01:12 +0200

    fix deploy

--- src/pages/Home/Homepage.tsx
+++ src/pages/Home/Homepage.tsx
@@ -1,12 +1,4 @@
-import {
-  Card,
-  CardAction,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
+import { Card, CardHeader } from "@/components/ui/card"
 import { useEffect, useState, useRef, useCallback } from "react";
 import styles from "./Homepage.module.scss";
 import { Button } from "@/components/ui/button";
